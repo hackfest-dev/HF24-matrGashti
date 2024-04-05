4729f0e46b0e5998a593bc98ebd568cdaede2e5d
Namratha8213 <nm788186@gmail.com>
2024-04-05 22:54:12 +0530

    added otp

--- matrGashthi/src/App.jsx
+++ matrGashthi/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navigation"
 import AboutComponent from "./components/about_us"
 import DummyList from "./components/dummylist"
 import FoodList from "./components/foodCom"
+import Otp from "./components/otp"
 export default function App(){
   return(
     <>
@@ -9,6 +10,7 @@ export default function App(){
     <DummyList/>
     <AboutComponent/>
     <FoodList/>
+    <Otp/>
     </>
   )
 }
