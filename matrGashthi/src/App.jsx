180fd291fa88be8aba18f77c72e6f0ca0b6f156d
Namratha8213 <nm788186@gmail.com>
2024-04-05 19:50:35 +0530

    added dummylistcomponent3

--- matrGashthi/src/App.jsx
+++ matrGashthi/src/App.jsx
@@ -1,7 +1,10 @@
 import Navbar from "./components/Navigation"
-
+import DummyList from "./components/dummylist"
 export default function App(){
   return(
+    <>
     <Navbar/>
+    <DummyList/>
+    </>
   )
 }
