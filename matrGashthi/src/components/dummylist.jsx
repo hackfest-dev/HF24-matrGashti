f3573eac006f4a87069cada19457dedc963f7793
Namratha8213 <nm788186@gmail.com>
2024-04-05 18:44:10 +0530

    added dummylistcomponent

---
+++ matrGashthi/src/components/dummylist.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const DummyList = () => {
+  return (
+    <div className="dummy-list">
+      <div className="dummy-item">
+        <img src="food1.jpg" alt="Food 1" />
+        <h3>Restaurant Name 1</h3>
+        <p>Some information about the food from Restaurant 1.</p>
+        <button>Order Now</button>
+      </div>
+      <div className="dummy-item">
+        <img src="food2.jpg" alt="Food 2" />
+        <h3>Restaurant Name 2</h3>
+        <p>Some information about the food from Restaurant 2.</p>
+        <button>Order Now</button>
+      </div>
+      <div className="dummy-item">
+        <img src="food3.jpg" alt="Food 3" />
+        <h3>Restaurant Name 3</h3>
+        <p>Some information about the food from Restaurant 3.</p>
+        <button>Order Now</button>
+      </div>
+      <div className="dummy-item">
+        <img src="food4.jpg" alt="Food 4" />
+        <h3>Restaurant Name 4</h3>
+        <p>Some information about the food from Restaurant 4.</p>
+        <button>Order Now</button>
+      </div>
+      <div className="dummy-item">
+        <img src="food5.jpg" alt="Food 5" />
+        <h3>Restaurant Name 5</h3>
+        <p>Some information about the food from Restaurant 5.</p>
+        <button>Order Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default DummyList;
