e110f9135057a6b8aeaa5e64a5cd91a8c8c293cb
SilveRSpark1 <akhil.manoj@hotmail.com>
2024-04-06 22:51:48 +0530

    Created a menu

---
+++ matrGashthi/src/pages/Menu.jsx
@@ -0,0 +1,56 @@
+
+import { useState } from 'react'
+import React from 'react'
+import Table from '../components/Table'
+import { Modal } from '../components/Modal'
+
+const Menu = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const [rows, setRows]= useState([
+        {menuitem:'Gobi Manchurian',quantity:'10'},
+        {menuitem:'Aloo Manchurian',quantity:'10'}
+    ]);
+    const [rowToEdit, setRowToEdit] = useState(null);
+
+    const handleDeleteRow = (targetIndex) => {
+        setRows(rows.filter((_,idx) => idx !== targetIndex));
+    };
+
+    const handleEditRow = (idx) => {
+        setRowToEdit(idx);
+        setModalOpen(true);
+    };
+
+    const handleSubmit = (newRow) => {
+        rowToEdit === null 
+        ?setRows([...rows, newRow]) 
+        :setRows(
+            rows.map((currRow, idx) => {
+                if (idx !== rowToEdit) return currRow;
+
+                return newRow;
+        })
+        );
+    };
+
+  return (
+    <div className='Menu'>
+        <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow}/>
+        <button className='btn' onClick={() => setModalOpen(true)}>
+            Add
+        </button>
+        {modalOpen && (
+        <Modal closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+        }}
+        onSubmit={handleSubmit} 
+        defaultValue={rowToEdit !== null && rows[rowToEdit]}
+         />
+        )}
+    </div>
+  )
+}
+
+export default Menu
