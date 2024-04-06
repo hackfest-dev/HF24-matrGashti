d4336b7a9832c696e09fb1c2a049c64afda914ec
9964728107 <msayeemahmed76@gmail.com>
2024-04-06 14:17:07 +0530

    ginger

--- matrGashthi/src/App.jsx
+++ matrGashthi/src/App.jsx
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import Booking from "./pages/Booking"
-import AddItem from "./components/AddItem"
-import Navbar from "./components/Navigation"
-import Home from "./pages/Ehome"
-import {Routes, Route} from 'react-router-dom';
-import Signup from './pages/Signup';
-// import Food from './pages/Food';
-
-export default function App(){
-  return(
-    <Routes>
-      <Route path='/' element={<Signup/>}/>
-      <Route path='/Home' element={<Home/>}/>
-      {/* <Route path='/Food' element={<Food/>}/> */}
-      <Route path='/Booking' element={<Booking/>}/>
-    </Routes>
-=======
 import Update from "./components/UpdateProfile"
 import Signup from "./pages/Signup"
 export default function App(){
@@ -25,6 +7,5 @@ export default function App(){
       <Signup/>
       
     </div>
->>>>>>> cfabb5112d78f784be7dba5a373407d5df066883
   )
 }
