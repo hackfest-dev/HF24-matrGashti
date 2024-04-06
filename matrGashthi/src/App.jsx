219bdd39b6a7ff9c0c40dc1a15b3956dbad898f4
9964728107 <msayeemahmed76@gmail.com>
2024-04-06 12:21:30 +0530

    location input for signup

--- matrGashthi/src/App.jsx
+++ matrGashthi/src/App.jsx
@@ -1,10 +1,13 @@
 import Profile from "./pages/Profile"
 import Reviews from "./pages/Reviews"
 import Update from "./components/UpdateProfile"
+import Signup from "./pages/Signup"
 export default function App(){
   return(
     <div>
-      <Update/>
+      {/* <Update/> */}
+      <Signup/>
+      
     </div>
   )
 }
