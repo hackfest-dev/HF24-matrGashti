4d3354f703ac5fac8f745ea32bd371d642ef1d54
Namratha8213 <nm788186@gmail.com>
2024-04-06 14:46:25 +0530

    Footer changed

--- matrGashthi/src/App.jsx
+++ matrGashthi/src/App.jsx
@@ -1,11 +1,13 @@
 import Update from "./components/UpdateProfile"
+import Home from "./pages/Home"
 import Signup from "./pages/Signup"
+import Footer from "./components/Footer"
 export default function App(){
   return(
     <div>
       {/* <Update/> */}
-      <Signup/>
-      
+      <Home/>
+      <Footer/>
     </div>
   )
 }
--- matrGashthi/src/components/Footer.jsx
+++ matrGashthi/src/components/Footer.jsx
@@ -1,20 +1,33 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
-    return (
-      <div className="bg-red-400 py-10">
-        <div className="container mx-auto px-8">
-          <div className="flex justify-between items-center">
-            <div className="flex flex-col text-white">
-              <h1 className="text-xl font-semibold">ReCuisine</h1>
-              <p className="text-sm">Let's reduce food wastage together</p>
-            </div>
-            <div className="flex space-x-8">
-              <a href="/" className="text-white text-sm hover:text-gray-200">About</a>
-              <a href="/" className="text-white text-sm hover:text-gray-200">Food</a>
-              <a href="/" className="text-white text-sm hover:text-gray-200">Donate</a>
+  return (
+    <div className="bg-red-400 py-10">
+      <div className="container mx-auto px-8">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col text-white">
+            <h1 className="text-xl font-semibold">ReCuisine</h1>
+            <p className="text-sm">Let's reduce food wastage together</p>
+          </div>
+          <div className="flex space-x-8">
+            <a href="/" className="text-white text-sm hover:text-gray-200">About</a>
+            <a href="/" className="text-white text-sm hover:text-gray-200">Food</a>
+            <a href="/" className="text-white text-sm hover:text-gray-200">Donate</a>
+            <div className="flex space-x-4">
+              <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebook} className="text-white text-lg hover:text-gray-200" />
+              </a>
+              <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faTwitter} className="text-white text-lg hover:text-gray-200" />
+              </a>
+              <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} className="text-white text-lg hover:text-gray-200" />
+              </a>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
